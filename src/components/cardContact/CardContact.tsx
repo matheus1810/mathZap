9da9646b8cbb0b4edfae9247d71a22ebc89f578d
matheus1810 <matheussouza1810@gmail.com>
2022-05-10 16:01:18 -0300

    components

---
+++ src/components/cardContact/CardContact.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components"
+
+export const CardContact=styled.div`
+
+`
---
+++ src/pages/styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const Container=styled.div`
+display:grid;
+grid-template-columns:.6fr 1fr;
+height:100vh;
+background-color:lightgray;
+`
+
+export const GridContainer=styled.div`
+display:grid;
+grid-template-columns:1fr;
+grid-template-rows:10vh 80vh 10vh;
+`
+
